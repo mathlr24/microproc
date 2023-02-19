21db3fd2f2cd5ee1c7e296245ed0791cdb135279
mathlr24 <125263698+mathlr24@users.noreply.github.com>
2023-02-19 16:55:32 +0100

    End basics instructions
    I added all the basics instructions (add, sub, mul, div, and, or, xor, shl, shr). 
    I also added the instructions to tests de registers or the numbers (slt, sle and seq).
    I have not tested all the instructions yet.
    I also created a fonction set_regs. This functions is used to set all the value of the registers according to them number (R0 = 0, R1 = 1, ..., R31 = 31);
    I created a fonction to view the registers. Now we just need to call it to show all the registers. 
    To finish I created a fonction run. This function is used to run the program. In fact, in the main we juste have this fonction run. 
    In this function, we first show all the values. 
    After that while running is at 1, we carry out the instructions. 
    The only one solution to stop the code is to put running at 0. We do it when we encounter the instruction 0x0000000. After that we put runnign at 0 (in the case 0). And we finish the program by showing alla the registers.

--- VM_C/main.c
+++ VM_C/main.c
@@ -14,18 +14,27 @@ int regs[ NUM_REGS ];
 int prog[] = { 0x9c00043, 0x9c00043, 0x9c00043, 0x0000000 };//Lire les instructions qui arrivent en entrée (Les mettres en 32bits pour tester)
 int data[] = {15, 74, 56, 1, 0, 3, 4, 4, 5, 5};
 int pc = 0;
+int clock_rate = 0;
 
 int fetch(){
     return prog[ pc++ ];
 }
 
 
-void init_regs(){
-    for (int i = 0; i<32; i++){
-        regs[i] = i++;
+void set_regs(){
+    for (int i=0; i<31; i++){
+        regs[i] = i;
     }
     return 0;
 }
+
+void view_regs(){
+    for (int i=0; i<31; i++){
+        printf("R%d : %d  ", i, regs[i]);
+    }
+    return 0;
+}
+
 
 int codeop = 0;
 int Ralpha = 0;
@@ -62,36 +71,197 @@ void evaluate(){
             printf("HALT \n");
             running = 0;
             break;
+
+                    //------------------------------------------------//
+                    //----------------                 ---------------//
+                    //--------------- BASICS FONCTIONS ---------------//
+                    //----------------                 ---------------//
+                    //------------------------------------------------//
+
+
         case 1:                 // ADD      We do an addition 
-            printf("Addtion :\n");
             if (immo == 1){
                 regs[Rbeta] = regs[Ralpha] + o;
+                printf("Addition : R%d and %d\n", regs[Ralpha], o);
             }
-            else regs[Rbeta] = regs[Ralpha] + regs[o];
+
+            else {
+                regs[Rbeta] = regs[Ralpha] + regs[o];
+                printf("Addition : R%d and R%d\n", regs[Ralpha], o);
+            }
+            clock_rate++;
             break;
-        case 2 :                //LOAD      We load the data in the reg    
+            
+        case 2:                 // Sub      We do a subtraction 
+            if (immo == 1){
+                regs[Rbeta] = regs[Ralpha] - o;
+                printf("Subtraction : R%d and %d\n", regs[Ralpha], o);
+            }
+            else {
+                regs[Rbeta] = regs[Ralpha] - regs[o];
+                printf("Subtraction : R%d and R%d\n", regs[Ralpha], o);
+            }
+            clock_rate++;
+            break;
+
+        case 3:                // Mul      We do a multiplication
+            if (immo == 1){
+                regs[Rbeta] = regs[Ralpha] * o;
+                printf("Multiplication : R%d and %d\n", regs[Ralpha], o);
+            }
+            else {
+                regs[Rbeta] = regs[Ralpha] * regs[o];
+                printf("Multiplication : R%d and R%d\n", regs[Ralpha], o);
+            }
+            clock_rate+=2;
+            break;
+
+        case 4:                // Div      We do a division
+            if (immo == 1){
+                regs[Rbeta] = regs[Ralpha] / o;
+                printf("Division : R%d and %d\n", regs[Ralpha], o);
+            }
+            else {
+                regs[Rbeta] = regs[Ralpha] / regs[o];
+                printf("Division : R%d and R%d\n", regs[Ralpha], o);
+            }
+            clock_rate+=2;
+            break;
+
+        case 5:                // And      We do the AND gate
+            if (immo == 1){
+                regs[Rbeta] = regs[Ralpha] & o;
+                printf("And gate : R%d and %d\n", regs[Ralpha], o);
+            }
+            else {
+                regs[Rbeta] = regs[Ralpha] & regs[o];
+                printf("And gate : R%d and R%d\n", regs[Ralpha], o);
+            }
+            clock_rate++;
+            break;
+
+        case 6:                // Or      We do the OR gate
+            if (immo == 1){
+                regs[Rbeta] = regs[Ralpha] | o;
+                printf("Or gate : R%d and %d\n", regs[Ralpha], o);
+            }
+            else {
+                regs[Rbeta] = regs[Ralpha] | regs[o];
+                printf("Or gate : R%d and R%d\n", regs[Ralpha], o);
+            }
+            clock_rate++;
+            break;
+
+        case 7:                // exclusive OR      We do an exclusive OR gate
+            if (immo == 1){
+                regs[Rbeta] = regs[Ralpha] ^ o;
+                printf("Exclusive Or : R%d and %d\n", regs[Ralpha], o);
+            }
+            else {
+                regs[Rbeta] = regs[Ralpha] ^ regs[o];
+                printf("Exclusive Or : R%d and R%d\n", regs[Ralpha], o);
+            }
+            clock_rate++;
+            break;
+
+        case 8:                // shl      We do a left shift
+            if (immo == 1){
+                regs[Rbeta] = regs[Ralpha] << o;
+                printf("Left shift : R%d and %d\n", regs[Ralpha], o);
+            }
+            else {
+                regs[Rbeta] = regs[Ralpha] << regs[o];
+                printf("Left shift : R%d and R%d\n", regs[Ralpha], o);
+            }
+            clock_rate++;
+            break;
+
+        case 9:                // shr      We do a right shift
+            if (immo == 1){
+                regs[Rbeta] = regs[Ralpha] >> o;
+                printf("Right shift : R%d and %d\n", regs[Ralpha], o);
+            }
+            else {
+                regs[Rbeta] = regs[Ralpha] >> regs[o];
+                printf("Right shift : R%d and R%d\n", regs[Ralpha], o);
+            }
+            clock_rate++;
+            break;
+                    //-------------------------------------------------------//
+                    //----------------                        ---------------//
+                    //--------------- LOWER / EQUAL FONCTIONS ---------------//
+                    //----------------                        ---------------//
+                    //-------------------------------------------------------//
+                    
+
+        case 10:                // slt      We look for 'set lower than'
+            if (immo == 1){
+                regs[Rbeta] = (regs[Ralpha] < o) ? 1 : 0;
+                printf("Set lower than : R%d and %d\n", regs[Ralpha], o);
+            }
+            else {
+                regs[Rbeta] = (regs[Ralpha] < regs[o]) ? 1 : 0;
+                printf("Set lower than : R%d and R%d\n", regs[Ralpha], o);
+            }
+            clock_rate++;
+            break;
+        
+        case 11:                // sle      We look for 'set lower equal'
+            if (immo == 1){
+                regs[Rbeta] = (regs[Ralpha] <= o) ? 1 : 0;
+                printf("Set lower equal : R%d and %d\n", regs[Ralpha], o);
+            }
+            else {
+                regs[Rbeta] = (regs[Ralpha] <= regs[o]) ? 1 : 0;
+                printf("Set lower equal : R%d and R%d\n", regs[Ralpha], o);
+            }
+            clock_rate++;
+            break;
+
+        case 11:                // seq      We look for 'set equal'
+            if (immo == 1){
+                regs[Rbeta] = (regs[Ralpha] == o) ? 1 : 0;
+                printf("Set equal : R%d and %d\n", regs[Ralpha], o);
+            }
+            else {
+                regs[Rbeta] = (regs[Ralpha] == regs[o]) ? 1 : 0;
+                printf("Set equal : R%d and R%d\n", regs[Ralpha], o);
+            }
+            clock_rate++;
+            break;
+
+
+                    //--------------------------------------------------------//
+                    //----------------                         ---------------//
+                    //--------------- LOAD and STORE FONCTIONS ---------------//
+                    //----------------                         ---------------//
+                    //--------------------------------------------------------//
+        
+
+        case 117 :                //LOAD      We load the data in the reg    
             printf("LOAD : \n");
             //regs[]
             break;
+        
 
 
     }
 
 }
 
+void run(){
+    view_regs();
+    while (running)
+    {
+        int instr = fetch();
+        decode(instr);
+        evaluate();
+    }
+    view_regs();
+    return 0;
+}
+
 int main() {
-    init_regs();
-    for (int i=0; i<31; i++){
-        printf("R%d : %d  ", i, regs[i]);
-    }
-    regs[2] = 2;
-    regs[3] = 4;
-    int mot = 0x08800064;
-    decode(mot);
-    evaluate();
-    for (int i=0; i<31; i++){
-        printf("R%d : %d  ", i, regs[i]);
-    }
-    
+    run();
     return 0;
 }
