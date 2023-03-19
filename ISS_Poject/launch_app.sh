7cc5f6c7dd2a4a15fcb5a59b218aecd96f1995d5
mathlr24 <125263698+mathlr24@users.noreply.github.com>
2023-03-19 22:08:10 +0100

    Delete launch_app.sh

--- ISS_Poject/launch_app.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-python3 assembler_code/translator.py tests_codes/assembleur.txt storage/traduced_file.bin
-gcc -o VM_in_c/ISS VM_in_c/ISS.c
-./VM_in_c/ISS storage/traduced_file.bin storage/data.bin
-
-#Script pour lancer l'ensemble du programme.
