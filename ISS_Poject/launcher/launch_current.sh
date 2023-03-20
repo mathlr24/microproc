2799df6015ab200fdb9d9a5b5911e60f9940ed74
mathlr24 <125263698+mathlr24@users.noreply.github.com>
2023-03-20 11:56:03 +0100

    Update launch_current.sh

--- ISS_Poject/launcher/launch_current.sh
+++ ISS_Poject/launcher/launch_current.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
-cd ..
-python3 assembler_code/translator.py tests_codes/code.txt storage/traduced_file.bin
-gcc -o VM_in_c/ISS VM_in_c/ISS.c
-./VM_in_c/ISS storage/traduced_file.bin storage/data.bin
 
-#Script pour lancer l'ensemble du programme.
+if [ $# -ne 2 ]; then
+    echo "Usage: $0 arg1 arg2"
+    echo "arg1 : assembler file to translate"
+    echo "arg2 : name of the data file"
+    exit 1
+fi
+
+cd ..			#We go back to the parent directory
+python3 assembler_code/translator.py $1 storage/traduced_file.bin
+gcc -o VM_in_c/ISS VM_in_c/ISS.c
+./VM_in_c/ISS storage/traduced_file.bin $2
+
+#Script to launch the whole program.
